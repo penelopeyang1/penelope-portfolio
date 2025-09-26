65a0a07b75a93faeac67aa775d2929a04ce05ef1
Penelope Yang <penelope.yang21@gmail.com>
2025-09-26 10:14:17 -0700

    Pixel art additions

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Landing.css';
-import Signature from '../Signature/Signature'; // Import the Signature component
+import Signature from '../Signature/Signature'; 
+import polaroidImage from '../../assets/pixel-polaroid.png';
 
 export default function Landing() {
   return (
     <section id="landing" className="landing-section">
       <div className="landing-content">
-        <p className="greeting-text">Hello, my name is</p>
-        {/* <Signature />  */}
-        <p className='name'>Penelope Yang</p>
-        <p className="intro">A software engineer building things she wished existed, combining creativity with experience in AI systems, full-stack apps, and business tools.</p>
+        <div className="landing-intro">
+          <p className="greeting-text">Hello, my name is</p>
+          {/* <Signature />  */}
+          <p className='name'>Penelope Yang</p>
+          <p className="description">A software engineer building things she wished existed, combining creativity with experience in AI systems, full-stack apps, and business tools.</p>
+        </div>
+        <div className="polaroid">
+          <img src={polaroidImage} alt="Polaroid" />
+        </div>
       </div>
     </section>
   );
