e082c012cc9eee72ad816ddf68ae22bce87a38e7
Penelope Yang <penelope.yang21@gmail.com>
2025-09-27 00:24:06 -0700

    Landing Page Sticky Note & Media Queries

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './Landing.css';
 import Signature from '../Signature/Signature'; 
 import polaroidImage from '../../assets/pixel-polaroid.png';
+import welcomeNote from '../../assets/welcome-note.png';
 
 export default function Landing() {
   return (
     <section id="landing" className="landing-section">
       <div className="landing-content">
+        <div className="welcome-note">
+          <img src={welcomeNote} alt="Welcome Note" />
+        </div>
         <div className="landing-intro">
           <p className="greeting-text">Hello, my name is</p>
           {/* <Signature />  */}
